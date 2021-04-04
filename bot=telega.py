03a13f9c92352a4123c1900c0b4ce56adfaa2336
Hatshot-gg <81925582+Hatshot-gg@users.noreply.github.com>
2021-04-04 23:18:39 +0500

    Add files via upload

---
+++ bot=telega.py
@@ -0,0 +1,222 @@
+import telebot
+
+bot = telebot.TeleBot('1625318010:AAGGsB-WVwhOGA7uwLFtyFKQpZMaX573mhE')
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if message.text.lower() == 'очки':
+        bot.send_message(message.chat.id, 'نَظَّارَةً')
+    if message.text.lower() == 'نَظَّارَةً':
+        bot.send_message(message.chat.id, 'очки')
+    if message.text.lower() == 'зима':
+        bot.send_message(message.chat.id, 'اَلشِّتَاءُ')
+    if message.text.lower() == 'اَلشِّتَاءُ':
+        bot.send_message(message.chat.id, 'зима')
+    if message.text.lower() == 'лето':
+        bot.send_message(message.chat.id, 'اَلصَّيْفُ')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'весна':
+        bot.send_message(message.chat.id, 'اَلرَّبِيعُ')
+    if message.text.lower() == 'اَلرَّبِيعُ':
+        bot.send_message(message.chat.id, 'весна')
+    if message.text.lower() == 'осень':
+        bot.send_message(message.chat.id, 'اَلْخَرِيفُ')
+    if message.text.lower() == 'اَلْخَرِيفُ':
+        bot.send_message(message.chat.id, 'осень')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+    if message.text.lower() == 'اَلصَّيْفُ':
+        bot.send_message(message.chat.id, 'лето')
+
+
+
+
+bot.polling(none_stop=True)
+
+
+
+
+
+
+
